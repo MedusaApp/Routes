f28f5f854155f8666db3c7917aef82952fe8731f
David Weiner <david.l.weiner30030@gmail.com>
2019-10-13 23:13:45 -0400

    Updated routes

--- routes/routes.php
+++ routes/routes.php
@@ -1,8 +1,5 @@
 <?php
 
-// Authentication routes, with email verification required
-Auth::routes(['verify' => true]);
-
 // Show membership pending page
 Route::get('/pending', function () {
     return view('personality::auth.pending');
